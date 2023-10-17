7596073dc3d37871b3720cb5cbe8cf1a29cf1712
TheMax567 <147410498+TheMax567@users.noreply.github.com>
2023-10-17 14:23:56 +0200

    Add files via upload

--- kladblok Max.R
+++ kladblok Max.R
@@ -2,47 +2,59 @@
 library(ranger)
 library(tidyverse)
 library(ggplot2)
-library(caret)
 library(bst)
+library(caret)
 
 test_data <- read_rds("data/test.rds")
 train_data <- read_rds("data/train.rds")
+
+set.seed(3456)
 
 fitControl <- trainControl(method = "repeatedcv",
                            number = 5,
                            repeats = 5)
 getModelInfo("ranger")
 HP_rndFrst <- expand.grid(mtry = 17,
-                               splitrule = "extratrees",
+                               splitrule = "variance",
                                min.node.size = 5)
 
-rndFrst <- train(score ~ PC1 + PC2 + PC3 + PC4 + PC5 + PC6,
+rndFrst <- train(score ~ .,
+                 data = train_data,
+                 method = "ranger",
+                 trControl = fitControl,
+                 tuneGrid = HP_rndFrst)
+summary(rndFrst)
+print(rndFrst)
+
+rndFrst_PCA <- train(score ~ .,
                  data = PCA_traindata,
                  method = "ranger",
                  trControl = fitControl,
                  tuneGrid = HP_rndFrst
                  )
-summary(rndFrst)
-print(rndFrst)
+summary(rndFrst_PCA)
+print(rndFrst_PCA)
 
-HP_bst <- expand.grid(maxdepth = 2,
-                      mstop = 50,
+HP_bst <- expand.grid(maxdepth = 1,
+                      mstop = 150,
                       nu = 0.1)
 
-bst <- train(score ~ PC1 + PC2 + PC3 + PC4 + PC5 + PC6,
-            data = PCA_traindata,
+bst <- train(score ~ .,
+            data = train_data,
             method = "bstTree",
             trControl = fitControl,
             tuneGrid = HP_bst)
 summary(bst)
 print(bst)
 
-lnrRgr <- train(score ~ PC1 + PC2 + PC3 + PC4 + PC5 + PC6,
+bst_PCA <- train(score ~ .,
              data = PCA_traindata,
-             method = "lm",
-             trControl = fitControl)
-summary(lnrRgr)
-print(lnrRgr)
+             method = "bstTree",
+             trControl = fitControl,
+             tuneGrid = HP_bst
+             )
+summary(bst_PCA)
+print(bst_PCA)
 
 predict(rndFrst,
         newdata = test_data)
